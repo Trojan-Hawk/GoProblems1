55d1fa69e69ab652e66f908099df4970085dda6e
TIMOTHY CASSIDY - Student <G00333333@gmit.ie>
2017-09-27 10:08:25 +0100

    First Commit

---
+++ GoFundamentals_Problem1.go
@@ -0,0 +1,15 @@
+// Student Name: Timothy Cassidy
+// Student Number: G00333333
+
+package main
+
+// format package import
+import "fmt"
+
+// Hello World main function
+func main() {
+	
+	// Formatted Print Line
+    fmt.Println("こんにちは世界")
+	
+}// main
